65f09a19d327466ff9a7ea34f77e09a5a0d6169e
SassonNir <nirsasson00@gmail.com>
2021-11-17 19:23:50 +0200

    Testing around the cases

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from Building import Building
 from Controller import Controller
 from Elevator import Elevator
 jsonPath = './data/Ex1_input/Ex1_Buildings/B5.json'
-csvPath = './data/Ex1_input/Ex1_Calls/Calls_d.csv'
+csvPath = './data/Ex1_input/Ex1_Calls/Calls_a.csv'
+# java -jar .\Ex1_checker_V1.2_obf.jar 123456789,123456789 .\data\Ex1_input\Ex1_Buildings\B5.json .\out.csv out.log
 
 debuging = True
 
@@ -13,6 +14,7 @@ def main():
     controller = Controller.from_csv(csvPath)
     controller.allocate(building)
     print(controller.allocated_elevators)
+    controller.to_csv('out.csv')
     pass
 
 
