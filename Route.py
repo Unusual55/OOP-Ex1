a770ca212cd049ea73bcd82163a50096dcbea192
Ofri Tavor <ofritavor55@gmail.com>
2021-11-19 20:12:49 +0200

    Updated the documentation

--- Route.py
+++ Route.py
@@ -5,8 +5,18 @@ from Elevator import Direction
 from Vector import Vector
 import functools
 from math import ceil, floor, copysign
-
+#The Route is The course management of the elevator, it's a data structure that contain the list
+#of Vector objects that the elevator take care of, a list of Node object which represent it's course
+#as well as it's time line for the simulation.
+#The Route also contain the important constant we need to take care of during the simulation:
+#1. start_course: The time that will take for the elevator to close the doors and start the engine
+#2. end_course: The time that will take for the elevator to stop at the wanted floor and open the doors
+#3. full_break: The time that will take to stop at wanted floor- the end of the last course and the
+#   beginning of a new course
+#4. speed: The speed of the elevator, how many floors the elevator pass in 1 second when it's moving
+#5. tpf: Time Per Floor- how much time it will take to move 1 floor(1/speed)
 class Route:
+    #This function initialize the Route object
     def __init__(self, e) -> None:
         import Elevator
         self.call_pointers = []
@@ -15,6 +25,9 @@ class Route:
         self.speed_const = {'speed': e.speed, 'tpf': 1/e.speed}
         self.count = 0
     
+    #This function get a vector and a course and check the number of case we need in order to
+    #know which case is needed to calculate the index of the src and dst nodes as well as how to
+    #calculate the time that will take to finish taking care of the call
     def case_check(self, vec: Vector, course: list):
         elev_dir = self.get_state(vec.incoming, course)
         call_dir = copysign(1.0, vec.src.floor - vec.dst.floor)
@@ -71,11 +84,13 @@ class Route:
                 else:
                     return 4
                     
+    #This function get a list of vectors and return the time that will take to finish all of the
+    #calls
     @staticmethod        
     def calc_route_time(vect: list):
         return sum([v.dst.time - v.incoming.time for v in vect])
 
-
+    #This function get a vector create a list that contain only the incoming nodes and return it
     def create_basic_course(self, vec:Vector):
         course = []
         course.insert(0,vec.incoming)
@@ -83,7 +98,8 @@ class Route:
             for i in range(len(self.call_pointers)):
                 course.append(self.call_pointers[i].incoming)
         return course
-    #Can be improved with threads
+    
+    #This function add the vector to the course and update every other node that will be delayed
     def add_vector_to_route(self, vec: Vector, course: list):
         course.insert(0,vec.incoming)
         info = self.get_insertion_index(vec, course)
@@ -96,8 +112,10 @@ class Route:
         course.insert(info[0], vec.src)
         course.insert(info[1], vec.dst)
     
-    #start thread in the beginning of and iteration - lock it after 
-    # @jit(noPython=True)
+    #This function get a Call object, create a vector from it and insert it to the course.
+    #Since the add_vector_to_route function update the arrival time of every needed node, the function
+    #will sort the timed_course by the times and as a result it will turn it to online-algorithm like
+    #course. The function will return the time that will take to finish all of the calls
     @functools.lru_cache(maxsize=128)
     def create_dummy_course(self, c: Call):
         vec = Vector(c)
@@ -106,6 +124,8 @@ class Route:
         self.timed_course.sort(key=lambda x :x.time)
         return self.calc_route_time(self.call_pointers)
 
+    #This function calculates the future position of the elevator based on the nodes that already
+    #exist in the course
     def future_position(self, incoming_node: Node, course: list):
         state = self.get_state(incoming_node, course)
         inc_index = course.index(incoming_node)
@@ -123,7 +143,13 @@ class Route:
         dt -= self.stop_const.get("start_course") # Add the time it takes to close the doors and start the elevator
         return last_floor + state * ceil((dt * self.speed_const.get("tpf")))
 
-    #This function calculates the time which take to this call to complete as well as how much delay factor will be caused if we add this call to the list
+    #This function calculates the first easy case when the call and the elevator have the same
+    #direction and we can reach the source floor without change the direction
+    #The function returns a tuple that contain:
+    #1. The index that we need to insert the src node to
+    #2. The index that we need to insert the dst node to
+    #3. The time that will take to reach the src node
+    #4. The time that will take to reach the dst node
     def easy_case_same_diretion(self, vec: Vector, course: list):
         pos = self.future_position(vec.incoming, course)
         inc_node, src_node, dst_node = vec.incoming, vec.src, vec.dst
@@ -148,6 +174,13 @@ class Route:
         delay_factor = (2*src_delay +dst_delay)*(self.stop_const.get('full_break')+extra)
         return (src_index, dst_index, src_time , delay_factor + dist)
     
+    #This function calculates the second easy case when the elevator is idle and it's position is the
+    #same as the src node floor
+    #The function returns a tuple that contain:
+    #1. The index that we need to insert the src node to
+    #2. The index that we need to insert the dst node to
+    #3. The time that will take to reach the src node
+    #4. The time that will take to reach the dst node
     def easy_case_idle_same_floor(self, vec: Vector, course: list):
         pos = vec.src.floor
         inc_node, src_node, dst_node = vec.incoming, vec.src, vec.dst
@@ -164,6 +197,15 @@ class Route:
         dist= (abs(dst-pos))*self.speed_const.get('tpf') + dst_delay*(self.stop_const.get('full_break')+extra)
         return (inc_index, inc_index, 0, dist)
 
+    #This function calculates the third easy case when the call and the elevator have different
+    #directions but the source floor is in the direction of the elevator.
+    #The elevator can reach the source floor, but it will need to change it's direction in order
+    #to reach the destenation floor.
+    #The function returns a tuple that contain:
+    #1. The index that we need to insert the src node to
+    #2. The index that we need to insert the dst node to
+    #3. The time that will take to reach the src node
+    #4. The time that will take to reach the dst node
     def easy_case_inverse_direction(self, vec: Vector, course: list):
         pos = self.future_position(vec.incoming, course)
         inc_node, src_node, dst_node = vec.incoming, vec.src, vec.dst
@@ -193,6 +235,15 @@ class Route:
         dist += (2*src + dst_delay + 1) * (self.stop_const.get('full_break')+extra)
         return(src_index, dst_index, src_time , dist)
 
+    #This function work as support function for any other cases.
+    #We analyzed the simulator and we noticed that an elevator could stuck in a loop inside the same
+    #floor if the elevator is at specific position, it was already given an cmd goto command, then
+    #if we allocate another call which have the same source floor as the elevator position and the
+    #elevator is still at the same floor(didn't finish the time that takes to start a course), then
+    #the elevator will finish the start_course time, and then stop at the floor which will take
+    #end_course and then start to leave the floor again which will take another start_course.
+    #This function check if a situation like this could happen and if it will happed, it returns the
+    #extra delay that will be caused because of the loop.
     def hard_case_analysis_stop(self, vec: Vector, course: list):
         pos = self.future_position(vec.incoming, course)
         inc_node, src_node, dst_node = vec.incoming, vec.src, vec.dst
@@ -214,8 +265,14 @@ class Route:
         delay_by_call = loopscount * self.stop_const.get('full_break') +cmd_delay
         return delay_by_call
 
-        #add cmd_delay to src.time at the end
-            
+    ##This function calculates the first hard case when the call and the elevator have the same
+    #direction but we need to change the direction of the elevator in order to reach the source floor
+    #and then change it again in order to reach the destenation floor.
+    #The function returns a tuple that contain:
+    #1. The index that we need to insert the src node to
+    #2. The index that we need to insert the dst node to
+    #3. The time that will take to reach the src node
+    #4. The time that will take to reach the dst node            
     def hard_case_missed_floor(self, vec: Vector, course: list):
         pos = self.future_position(vec.incoming, course)
         inc_node, src_node, dst_node = vec.incoming, vec.src, vec.dst
@@ -245,6 +302,7 @@ class Route:
         delay_factor = (2*src_delay + dst_delay)*(self.stop_const.get('full_break')+extra)
         return (src_index, dst_index, src_time , dist + delay_factor)
 
+    #This fuction returns a sorted  of Nodes that sorted by the time property of it's values
     def get_sorted_nodelist(self):
         li = [] 
         for i in self.call_pointers:
@@ -252,6 +310,8 @@ class Route:
             li.append(i[1])
         return sorted(li, key=lambda x: x.time, reverse=True)
 
+    #This function get an index and a course, and returns the index of the last floor that have the
+    #same direction as the elevator, and after it the direction will change
     def find_turning_point(self, i: int, course: list):
         calls = course[i:]
         if len(calls) < 2:
@@ -263,18 +323,14 @@ class Route:
                 return i + j + 1
             sign = s
         return -1
-    #Can be improved with threads
+
+    #This function get an incoming Node and a course and return the state of the elevator at the time
+    #of the incoming Node
     def get_state(self, incoming: Node, course: list):
         inc_index = course.index(incoming)
         if inc_index == len(course)-1:
             return Direction.IDLE
         i = inc_index
-        # while i-1>0 and course[i].type == Type.incoming:
-        #     i -=1
-        # last_Stop_floor = course[i].floor
-        # j = inc_index
-        # while j+1< len(course) and course[j].type == Type.incoming:
-        #     j += 1
         pre1 = [node for node in course if node.time< incoming.time]    
         pre = [node for node in pre1 if node.type!= Type.incoming]         
         post1 = [node for node in course if node.time> incoming.time]
@@ -287,6 +343,8 @@ class Route:
             return Direction.UP
         return Direction.DOWN
 
+    #This function get a vector and a course and return a tuple that returns from the cases calculation
+    #function and return it.
     def get_insertion_index(self, vec: Vector, course: list):
         case = self.case_check(vec, course)
         if case == 0:
@@ -298,12 +356,13 @@ class Route:
             tup = self.easy_case_inverse_direction(vec, course)
         elif case == 3:
             tup = self.hard_case_missed_floor(vec, course)
-        elif case == 4:
+        else:    
             tup = self.easy_case_idle_same_floor(vec, course)
-        else:
-            print("hello")
         return tup
-        
+    
+    #This function get a call and calculate how much time will take in order to finish taking care
+    #of the call and how much delay might be caused to all of the other calls and return a sum
+    #of the delay factor and finish time
     def check_insertion_delay_factor(self, c: Call):
         vec = Vector(c)
         self.timed_course.insert(0,vec.incoming)
